d4e7dce209df8fd7be83cb2fdcaf29715b46bccf
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-07-24 12:41:33 +0900

    プレイヤーを表示させる

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate termion;
+use std::io::{stdout, Write};
+use termion::raw::IntoRawMode;
+use termion::{async_stdin, clear, cursor, style};
+
 fn main() {
-    println!("Hello, world!");
+  let stdout = stdout();
+  let mut stdout = stdout.lock().into_raw_mode().unwrap();
+  let stdin = async_stdin();
+
+  write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+  stdout.flush().unwrap();
+
+  write!(stdout, "{}{}", clear::All, style::Reset);
+  write!(stdout, "{}", "@").unwrap();
+  stdout.flush().unwrap();
 }
