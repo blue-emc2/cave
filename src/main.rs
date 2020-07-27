8d838501c35676a04dfbe9a4fc87bcd65d2eb558
Shunsuke Yamazaki <s.yamazaki@everyleaf.com>
2020-07-27 22:10:30 +0900

    termionをやめてsdlで実装する方式に変更

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,38 @@
-extern crate termion;
-use crate::termion::input::TermRead;
-use std::io::{stdin, stdout, Write};
-use termion::event::Key::{Char, Down, Left, Right, Up};
-use termion::raw::IntoRawMode;
-use termion::{clear, cursor};
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use std::time::Duration;
 
 fn main() {
-  let stdout = stdout();
-  let mut stdout = stdout.lock().into_raw_mode().unwrap();
-  let stdin = stdin();
-  let stdin = stdin.lock();
+  let sdl_context = sdl2::init().unwrap();
+  let video_subsystem = sdl_context.video().unwrap();
 
-  write!(stdout, "{}", clear::All).unwrap();
-  write!(stdout, "{}", cursor::Hide).unwrap();
-  stdout.flush().unwrap();
-
-  write!(stdout, "{} x={}, y={}", cursor::Goto(1, 10), 1, 1).unwrap();
-  stdout.flush().unwrap();
-
-  let mut keys = stdin.keys();
-  let mut x: isize = 1;
-  let mut y: isize = 1;
-  let mut pos = (x, y);
-
-  loop {
-    // Debug
-    write!(stdout, "{} x={}, y={}", cursor::Goto(1, 10), pos.0, pos.1).unwrap();
-    // stdout.flush().unwrap();
-
-    let b = keys.next().unwrap().unwrap();
-
-    if Char('q') == b {
-      return;
-    }
-
-    let accel = match b {
-      Up => (0, -1),
-      Down => (0, 1),
-      Left => (-1, 0),
-      Right => (1, 0),
-      _ => (0, 0),
-    };
-
-    x += accel.0;
-    y += accel.1;
-
-    if 1 > x {
-      x = 1;
-    } else if 1 > y {
-      y = 1;
-    }
-
-    pos.0 = x;
-    pos.1 = y;
-
-    write!(
-      stdout,
-      "{}{}",
-      "@",
-      cursor::Goto(pos.0 as u16, pos.1 as u16)
-    )
+  let window = video_subsystem
+    .window("SDL", 640, 480)
+    .position_centered()
+    .build()
     .unwrap();
-    stdout.flush().unwrap();
+
+  let mut canvas = window.into_canvas().build().unwrap();
+  canvas.set_draw_color(Color::RGB(255, 255, 255));
+  canvas.clear();
+  canvas.present();
+
+  let mut event_pump = sdl_context.event_pump().unwrap();
+  'running: loop {
+    for event in event_pump.poll_iter() {
+      match event {
+        Event::Quit { .. }
+        | Event::KeyDown {
+          keycode: Some(Keycode::Escape),
+          ..
+        } => break 'running,
+        _ => {}
+      }
+    }
+
+    canvas.present();
+
+    ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
   }
 }
